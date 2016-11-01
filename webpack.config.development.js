6920d67ff6593a38d967ce240b7c5c929f01786e
Rob Marlow <robertjmarlow@gmail.com>
2016-10-31 20:51:11 -0500

    Adding some webpack configs

---
+++ webpack.config.development.js
@@ -0,0 +1,39 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    app: ['webpack/hot/dev-server', './src/jsx/entry.jsx'],
+  },
+
+  output: {
+    path: './public/built',
+    filename: 'bundle.js',
+    publicPath: 'http://localhost:8080/built/'
+  },
+
+  devServer: {
+    contentBase: './public',
+    publicPath: 'http://localhost:8080/built/'
+  },
+
+  // enable sourcemaps for debugging webpack's output.
+  devtool: 'source-map',
+
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+
+  module: {
+    loaders: [
+      { test: /\.jsx?$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.css$/, loader: 'style-loader!css-loader' }
+    ]
+  },
+
+  // this tells webpack that we're building an electron app
+  target: 'electron',
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
+}
---
+++ webpack.config.prod.js
@@ -0,0 +1,29 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    app: ['babel-polyfill', './src/jsx/entry.jsx'],
+  },
+
+  output: {
+    filename: 'bundle.js',
+    path: './public/built'
+  },
+
+  // enable sourcemaps for debugging webpack's output.
+  devtool: 'source-map',
+
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+
+  module: {
+    loaders: [
+      { test: /\.jsx?$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.css$/, loader: 'style-loader!css-loader' }
+    ]
+  },
+
+  // this tells webpack that we're building an electron app
+  target: 'electron',
+}
